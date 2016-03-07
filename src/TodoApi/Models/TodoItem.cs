a3843d990ce4e5136a7ea1f1ff86a548bef90cc7
Josh <jjbrow10@asu.edu>
2016-03-07 15:46:02 -0800

    Commit of API code

---
+++ src/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Priority { get; set; }
+        public string DueDate { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
